a36126a9f71b92c6bdbb978105b8e79376f0025d
James Rosenthal <james@ufn.com>
2014-01-13 21:49:57 -0500

    How’dyalike dem apples

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/playexplore.css" />
+    <link rel="stylesheet" href="css/master.css">
+    <script src="js/modernizr.js"></script>
+  </head>
+
+<body>
+
 <?php
 session_start();
 
@@ -10,40 +24,18 @@ if($_SESSION["login"]==1){
 }
 ?>
 
-<head>
-
-<style>
-.center{
-   width: 350px;
-   height: 30px;
-   border: 1px solid #999999;
-   padding: 5px;
-   position: absolute;
-   left: 50%;
-   top: 50%;
-   margin-left: -150px;
-   margin-top: -150px;
-}
-
-.submit{
- // width: 100px;
-  position: absolute;
-//  left: 50%;
-//  top: 70%
-  margin-left: -150px;
-  margin-top: -150px;
-}
-</style>
-
-</head>
-
-
-
-
 <form action="login.php" method="post">
 <input type="password" name="pword" class="center" autofocus>
 
 </br>
 
-<input type="submit" class="submit" >
+<input type="submit" class="submit button success" >
 </form>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
--- playexplore.php
+++ playexplore.php
@@ -1,20 +1,13 @@
-<?php
-session_start();
-if($_SESSION["login"]==1){
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mstream Media Player - Brainchild of Paul Sori</title>
 
-}
-else{
-	header("Location: index.php");
-	exit;
-}
+    <script src="js/modernizr.js"></script>
 
-?>
-
-<head>
-
-<link rel="stylesheet" type="text/css" href="/mstream/css/playexplore.css" />
-
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 
 <!-- The following are all for jplayer -->
 <link href="/mstream/jPlayer24/skin/prettify-jPlayer.css" rel="stylesheet" type="text/css" />
@@ -22,6 +15,8 @@ else{
 <script type="text/javascript" src="/mstream/jPlayer24/jquery.jplayer.min.js"></script>
 <script type="text/javascript" src="/mstream/jPlayer24/add-on/jplayer.playlist.min.js"></script>
 
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/master.css">
 
 <script type="text/javascript">
 $(document).ready(function(){
@@ -198,81 +193,144 @@ $(document).ready(function(){
 });
 
 </script>
-</head>
-
-
+  </head>
 
 <body>
 
+<?php
+	session_start();
+	if($_SESSION["login"]==1){
 
-	<div class='masterlist' id='filelist'>
-		<div class="filez">beanz</div>
-	</div>
+	}
+	else{
+		header("Location: index.php");
+		exit;
+	}
+?>
 
+<body>
 
-	<input type="hidden" id="currentdir"></input>
-	<input type="hidden" id="currentdirlong"></input>
+<div class="off-canvas-wrap">
+  <div class="inner-wrap">
+    <nav class="tab-bar">
 
+      <section class="left-small">
+        <a class="left-off-canvas-toggle menu-icon" ><span></span></a>
+      </section>
 
-	<div class='controls' id='controls'>
-		<div id='addall'>add directory</div>
-	</div>
+      <section class="right tab-bar-section">
+        <h1 class="title"><img src="img/mstream-logo.png" class="logo" alt="MStream" width="181" height="auto"></h1>
+      </section>
 
+    </nav>
 
+    <!-- Off Canvas Menu -->
+    <aside class="left-off-canvas-menu">
+      <ul class="off-canvas-list">
+        <li><label>Subdirectories(?)</label></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        ...
+      </ul>
+    </aside>
 
-	<div id="jp_container_N" class="jp-video jp-video-270p">
-		<div class="jp-type-playlist">
-			<div id="jquery_jplayer_N" class="jp-jplayer" style="width: 480px; height: 270px;"><img id="jp_poster_0" src="http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png" style="width: 480px; height: 270px; display: inline;"><audio id="jp_audio_0" preload="metadata" src="http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"></audio><video id="jp_video_0" preload="metadata" style="width: 0px; height: 0px;"></video></div>
-			<div class="jp-gui" style="">
-				<div class="jp-video-play" style="display: none;">
-					<a href="javascript:;" class="jp-video-play-icon" tabindex="1">play</a>
+    <section class="main-section">
+      <!-- content goes in here -->
+    	<div class="row">
+	
+		<div class="large-6 columns">
+			<h3>Your Library</h3>
+			<div class='masterlist' id='filelist'>
+				<div class="filez">beanz</div>
+			</div>
+		</div><!-- /6 columns -->
+
+		<div class="large-6 columns">
+
+				<input type="hidden" id="currentdir"></input>
+				<input type="hidden" id="currentdirlong"></input>
+				
+				<h3>Your Playlist</h3>
+				
+				<div class='controls' id='controls'>
+					<div id='addall' class="add_all">add directory</div>
 				</div>
-				<div class="jp-interface">
-					<div class="jp-progress">
-						<div class="jp-seek-bar" style="width: 100%;">
-							<div class="jp-play-bar" style="width: 0%;"></div>
+
+				<div id="jp_container_N" class="jp-video jp-video-270p">
+					<div class="jp-type-playlist">
+						<div id="jquery_jplayer_N" class="jp-jplayer" style="width: 480px; height: 270px;"><img id="jp_poster_0" src="http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png" style="width: 480px; height: 270px; display: inline;"><audio id="jp_audio_0" preload="metadata" src="http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"></audio><video id="jp_video_0" preload="metadata" style="width: 0px; height: 0px;"></video></div>
+						<div class="jp-gui" style="">
+							<div class="jp-video-play" style="display: none;">
+								<a href="javascript:;" class="jp-video-play-icon" tabindex="1">play</a>
+							</div>
+							<div class="jp-interface">
+								<div class="jp-progress">
+									<div class="jp-seek-bar" style="width: 100%;">
+										<div class="jp-play-bar" style="width: 0%;"></div>
+									</div>
+								</div>
+								<div class="jp-current-time">00:00</div>
+								<div class="jp-duration">04:27</div>
+								<div class="jp-controls-holder">
+									<ul class="jp-controls">
+										<li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
+										<li><a href="javascript:;" class="jp-play" tabindex="1" style="">play</a></li>
+										<li><a href="javascript:;" class="jp-pause" tabindex="1" style="display: none;">pause</a></li>
+										<li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
+										<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+										<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute" style="">mute</a></li>
+										<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute" style="display: none;">unmute</a></li>
+										<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume" style="">max volume</a></li>
+									</ul>
+									<div class="jp-volume-bar" style="">
+										<div class="jp-volume-bar-value" style="width: 80%;"></div>
+									</div>
+									<ul class="jp-toggles">
+										<li><a href="javascript:;" class="jp-full-screen" tabindex="1" title="full screen" style="">full screen</a></li>
+										<li><a href="javascript:;" class="jp-restore-screen" tabindex="1" title="restore screen" style="display: none;">restore screen</a></li>
+										<li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle" style="">shuffle</a></li>
+										<li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off" style="display: none;">shuffle off</a></li>
+										<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat" style="">repeat</a></li>
+										<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off" style="display: none;">repeat off</a></li>
+									</ul>
+								</div>
+								<div class="jp-title" style="display: none;">
+									<ul>
+										<li>Cro Magnon Man <span class="jp-artist">by The Stark Palace</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="jp-playlist">
+							<ul style="display: block;"><li class="jp-playlist-current"><div><a href="javascript:;" class="jp-playlist-item-remove" style="">×</a><a href="javascript:;" class="jp-playlist-item jp-playlist-current" tabindex="1">Cro Magnon Man <span class="jp-artist">by The Stark Palace</span></a></div></li></ul>
+						</div>
+						<div class="jp-no-solution" style="display: none;">
+							<span>Update Required</span>
+							To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
 						</div>
 					</div>
-					<div class="jp-current-time">00:00</div>
-					<div class="jp-duration">04:27</div>
-					<div class="jp-controls-holder">
-						<ul class="jp-controls">
-							<li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
-							<li><a href="javascript:;" class="jp-play" tabindex="1" style="">play</a></li>
-							<li><a href="javascript:;" class="jp-pause" tabindex="1" style="display: none;">pause</a></li>
-							<li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
-							<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-							<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute" style="">mute</a></li>
-							<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute" style="display: none;">unmute</a></li>
-							<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume" style="">max volume</a></li>
-						</ul>
-						<div class="jp-volume-bar" style="">
-							<div class="jp-volume-bar-value" style="width: 80%;"></div>
-						</div>
-						<ul class="jp-toggles">
-							<li><a href="javascript:;" class="jp-full-screen" tabindex="1" title="full screen" style="">full screen</a></li>
-							<li><a href="javascript:;" class="jp-restore-screen" tabindex="1" title="restore screen" style="display: none;">restore screen</a></li>
-							<li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle" style="">shuffle</a></li>
-							<li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off" style="display: none;">shuffle off</a></li>
-							<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat" style="">repeat</a></li>
-							<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off" style="display: none;">repeat off</a></li>
-						</ul>
 					</div>
-					<div class="jp-title" style="display: none;">
-						<ul>
-							<li>Cro Magnon Man <span class="jp-artist">by The Stark Palace</span></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="jp-playlist">
-				<ul style="display: block;"><li class="jp-playlist-current"><div><a href="javascript:;" class="jp-playlist-item-remove" style="">×</a><a href="javascript:;" class="jp-playlist-item jp-playlist-current" tabindex="1">Cro Magnon Man <span class="jp-artist">by The Stark Palace</span></a></div></li></ul>
-			</div>
-			<div class="jp-no-solution" style="display: none;">
-				<span>Update Required</span>
-				To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-			</div>
-		</div>
-		</div>
+		</div><!-- /6 columns -->
+	</div><!-- /row -->
+    </section>
 
-</body>
+  <!-- close the off-canvas menu -->
+  <a class="exit-off-canvas"></a>
+
+  </div>
+</div>
+
+
+	
+    <script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
